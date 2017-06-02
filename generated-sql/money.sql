5de200230b11b1973a0878502de287e551cc5a00
Christopher Wilkinson <git@wilk.tech>
2017-06-02 15:00:23 +0100

    Fix initial tests

--- generated-sql/money.sql
+++ generated-sql/money.sql
@@ -32,11 +32,16 @@ DROP TABLE IF EXISTS `breakdown`;
 CREATE TABLE `breakdown`
 (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `transaction_id` INTEGER NOT NULL,
     `description` VARCHAR(50) NOT NULL,
     `value` FLOAT NOT NULL,
     `category_id` INTEGER NOT NULL,
     PRIMARY KEY (`id`),
+    INDEX `breakdown_fi_98bea7` (`transaction_id`),
     INDEX `breakdown_fi_904832` (`category_id`),
+    CONSTRAINT `breakdown_fk_98bea7`
+        FOREIGN KEY (`transaction_id`)
+        REFERENCES `transaction` (`id`),
     CONSTRAINT `breakdown_fk_904832`
         FOREIGN KEY (`category_id`)
         REFERENCES `category` (`id`)
@@ -58,6 +63,40 @@ CREATE TABLE `category`
     CONSTRAINT `category_fk_474870`
         FOREIGN KEY (`account_id`)
         REFERENCES `account` (`id`)
+) ENGINE=InnoDB;
+
+-- ---------------------------------------------------------------------
+-- hashtag
+-- ---------------------------------------------------------------------
+
+DROP TABLE IF EXISTS `hashtag`;
+
+CREATE TABLE `hashtag`
+(
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `tag` VARCHAR(50) NOT NULL,
+    PRIMARY KEY (`id`),
+    UNIQUE INDEX `hashtag_u_24e05a` (`tag`)
+) ENGINE=InnoDB;
+
+-- ---------------------------------------------------------------------
+-- transaction_hashtag
+-- ---------------------------------------------------------------------
+
+DROP TABLE IF EXISTS `transaction_hashtag`;
+
+CREATE TABLE `transaction_hashtag`
+(
+    `transaction_id` INTEGER NOT NULL,
+    `hashtag_id` INTEGER NOT NULL,
+    PRIMARY KEY (`transaction_id`,`hashtag_id`),
+    INDEX `transaction_hashtag_fi_8d04e3` (`hashtag_id`),
+    CONSTRAINT `transaction_hashtag_fk_98bea7`
+        FOREIGN KEY (`transaction_id`)
+        REFERENCES `transaction` (`id`),
+    CONSTRAINT `transaction_hashtag_fk_8d04e3`
+        FOREIGN KEY (`hashtag_id`)
+        REFERENCES `hashtag` (`id`)
 ) ENGINE=InnoDB;
 
 -- ---------------------------------------------------------------------
---
+++ tests/Data/TestData.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Tests\Data;
+
+class TestData
+{
+  public static function populateDatabase()
+  {
+    $account = new \Account();
+    $account->setName('Cash');
+    $account->save();
+
+    $account = new \Account();
+    $account->setName('Bank');
+    $account->save();
+
+    $user = new \User();
+    $user->setAccount($account);
+    $user->setFirstName('Bob');
+    $user->setLastName('Jones');
+    $user->setEmail('Jones');
+    $user->setEnable(true);
+    $user->setPassword('really-secure');
+    $user->save();
+
+    $hashtagTest = new \Hashtag();
+    $hashtagTest->setTag('test');
+    $hashtagTest->save();
+
+    $hashtagSomething = new \Hashtag();
+    $hashtagSomething->setTag('something');
+    $hashtagSomething->save();
+
+    $transaction = new \Transaction();
+    $transaction->setDate(new \DateTime());
+    $transaction->setValue(1.05);
+    $transaction->setAccount($account);
+    $transaction->setDescription('#test description');
+    $transaction->addHashtag($hashtagTest);
+    $transaction->save();
+
+    $transaction = new \Transaction();
+    $transaction->setDate(new \DateTime('- 3 months'));
+    $transaction->setValue(-403.86);
+    $transaction->setAccount($account);
+    $transaction->setDescription('this is #something expensive');
+    $transaction->addHashtag($hashtagSomething);
+    $transaction->save();
+  }
+}

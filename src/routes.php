814908749c242cc0652d766e9c5f8bfb9a86ff48
Christopher Wilkinson <git@wilk.tech>
2017-06-02 17:02:56 +0100

    Add simple auth

--- src/routes.php
+++ src/routes.php
@@ -245,9 +245,84 @@ $app->get('/fixHashtags', function ($request, $response, $args) {
 });
 */
 
-$app->get('/[{name}]', function ($request, $response, $args) {
+
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// AUTH
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+$app->get('/login', function ($request, $response, $args) {
+
+    $this->logger->info("Fetch login GET '/login'");
+
+    if (isset($_SESSION['userId']))
+    {
+        return $response->withStatus(302)->withHeader('Location', $this->router->pathFor('home'));
+    }
+
+    return $this->view->render($response->withStatus(401), 'login.twig' );
+})->setName('login');
+
+
+$app->post('/login', function ($request, $response, $args) {
+
+    $this->logger->info("Login POST '/login'");
+
+    $message = "Username or password incorrect.";
+
+    $data = $request->getParsedBody();
+
+    try {
+        $email = new EmailAddress($data['username']);
+    } catch (InvalidArgumentException $e) {
+        return $this->view->render($response->withStatus(401), 'login.twig', ['message' => $message] );
+    }
+    $password = filter_var($data['password'], FILTER_SANITIZE_STRING);
+
+    if ($email == "" || $password == "")
+    {
+        return $this->view->render($response->withStatus(401), 'login.twig', ['message' => $message] );
+    }
+
+    // login
+    $auth = new Authentication($this);
+    try
+    {
+        if ($auth->loginAttempt($email, $password))
+        {
+            if (isset($_SESSION['urlRedirect']))
+            {
+                $url = $_SESSION['urlRedirect'];
+                unset($_SESSION['urlRedirect']);
+                return $response->withStatus(303)->withHeader('Location', $url);
+            }
+            return $response->withStatus(303)->withHeader('Location', $this->router->pathFor('home'));
+        }
+    }
+    catch (\Exception $e)
+    {
+        $message = "Too many failed login attempts. Please try again in 15 minutes.";
+    }
+    return $this->view->render($response->withStatus(401), 'login.twig', ['username' => $email, 'message' => $message ] );
+})->setName('login-post');
+
+
+$app->get('/logout', function ($request, $response, $args) {
+
+    $this->logger->info("Fetch logout GET '/logout'");
+
+    unset($_SESSION['userId']);
+
+    return $response->withStatus(302)->withHeader('Location', $this->router->pathFor('login'));
+})->setName('logout');
+
+
+
+
+$app->get('/', function ($request, $response, $args) {
     // Sample log message
-    $this->logger->info("Slim-Skeleton '/' route");
+    $this->logger->info("GET dashboard '/' route");
 
     // Render index view
     return $this->renderer->render($response, 'index.phtml', [ "router" => $this->router ] );

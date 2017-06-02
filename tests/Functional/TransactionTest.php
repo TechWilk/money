19b491ce8c03b546ff097ade53c47cecae19da44
Christopher Wilkinson <git@wilk.tech>
2017-06-02 18:33:01 +0100

    Expand transaction tests

--- tests/Functional/TransactionTest.php
+++ tests/Functional/TransactionTest.php
@@ -18,19 +18,64 @@ class TransactionTest extends BaseTestCase
         $this->assertContains('Save', (string)$response->getBody());
     }
 
+    public function providerTestPostTransaction()
+  {
+    return [
+      [ (new \DateTime())->format('Y-m-d'), 1.05, 'income', '#something with some #hashtags', 1 ],
+      [ (new \DateTime('-1 week'))->format('Y-m-d'), 5.89, 'outgoings', '#other #hashtags', 1 ],
+      [ (new \DateTime('+2 months'))->format('Y-m-d'), '5.89 + 4.98', 'outgoings', 'I\'ve done some #shopping', 1 ],
+      [ (new \DateTime('-10 months'))->format('Y-m-d'), '5*4.99 + 3.97 - 3.1', 'income', 'please do not #error', 1 ],
+    ];
+  }
+
     /**
+     * @param string $email
+     * @param mixed $value
+     * @param string $direction
+     * @param string $description
+     * @param int $account
+     *
+     * @dataProvider providerTestPostTransaction
      * Test that the hashtags route returns a rendered response containing the text 'All Hashtags' and 'Jump to'
      */
-    public function testPostTransaction()
+    public function testPostTransaction($date, $value, $direction, $description, $account)
     {
         $response = $this->runApp('POST', '/transaction', [ 
-            'date' => (new \DateTime())->format('Y-m-d'),
-            'value' => 1.05,
-            'direction' => 'income',
-            'description' => '#something with some #hashtags',
-            'account' => [ 1 ],
+            'date' => $date,
+            'value' => $value,
+            'direction' => $direction,
+            'description' => $description,
+            'account' => [ $account ], // in array, simulating <select>
         ]);
 
         $this->assertEquals(302, $response->getStatusCode());
+    }
+
+    public function testPostExistingTransaction()
+    {
+        $date = (new \DateTime())->format('Y-m-d');
+        $value = 5.89;
+        $direction = 'outgoings';
+        $description = 'not #another description';
+        $account = 1;
+
+        $response = $this->runApp('POST', '/transaction/1', [ 
+            'date' => $date,
+            'value' => $value,
+            'direction' => $direction,
+            'description' => $description,
+            'account' => [ $account ], // in array, simulating <select>
+        ]);
+
+        $this->assertEquals(302, $response->getStatusCode());
+    }
+
+    public function testGetExistingTransactionDetails()
+    {
+        $response = $this->runApp('GET', '/transaction/1');
+
+        $this->assertEquals(200, $response->getStatusCode());
+        $this->assertContains('Transaction', (string)$response->getBody());
+        $this->assertContains('edit', (string)$response->getBody());
     }
 }

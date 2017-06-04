d91bef3909fa33a4ce0bb5f2e42bf4153839056d
Christopher Wilkinson <git@wilk.tech>
2017-06-04 17:19:44 +0100

    Use UTC timezone for login attempt lockout

--- src/classes/Authentication.php
+++ src/classes/Authentication.php
@@ -58,7 +58,7 @@ class Authentication
   {
     if (!$this->numberOfLoginAttemptsIsOk($email))
     {
-      throw new Exception('Too many attempts.');
+      throw new \Exception('Too many attempts.');
       return false;
     }
     $users = UserQuery::create()->filterByEmail($email)->find();
@@ -82,7 +82,10 @@ class Authentication
     $numberOfAllowedAttempts = 8;
     $lockOutInterval = 15; // mins
 
-    $loginFailures = LoginFailureQuery::create()->filterByUsername($username)->filterByTimestamp(['min' => new DateTime("-$lockOutInterval minutes")])->count();
+    $date = new DateTime("-$lockOutInterval minutes");
+    $date->setTimezone(new DateTimeZone("UTC"));
+
+    $loginFailures = LoginFailureQuery::create()->filterByUsername($username)->filterByTimestamp(['min' => $date ])->count();
 
     if ($loginFailures < $numberOfAllowedAttempts) {
       return true;

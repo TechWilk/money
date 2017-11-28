71a1fc58be527ce6ea52a91a2ede1d997b216967
Christopher Wilkinson <christopher.wilkinson@eden.co.uk>
2017-11-28 12:38:00 +0000

    Group routes paths

--- src/routes.php
+++ src/routes.php
@@ -6,26 +6,32 @@ use TechWilk\Money\Controller\AuthController;
 use TechWilk\Money\Controller\DashboardController;
 use TechWilk\Money\Controller\TagController;
 use TechWilk\Money\Controller\TransactionController;
-use TechWilk\Money\Controller\UserController;
+use DateTime;
 
 // Routes
 $app->get('/', DashboardController::class.':getMainDashboard')->setName('home');
 
-$app->post('/user', UserController::class.':postUser')->setName('user-new-post');
-$app->get('/user/new', UserController::class.':getUserNew')->setName('user-new-post');
-$app->post('/user/{id}/password', UserController::class.':postUserPassword')->setName('user-password-post');
-$app->get('/user/{id}', UserController::class.':getUser')->setName('user');
+$app->group('/user', function() {
+    $this->post('', UserController::class.':postUser')->setName('user-new-post');
+    $this->get('/new', UserController::class.':getUserNew')->setName('user-new-post');
+    $this->post('/{id}/password', UserController::class.':postUserPassword')->setName('user-password-post');
+    $this->get('/{id}', UserController::class.':getUser')->setName('user');
+});
 
-$app->get('/tags', TagController::class.':getTags')->setName('tags');
-$app->get('/tags.json', TagController::class.':getTagsJson')->setName('tags-json');
-$app->get('/tag/{tag}', TagController::class.':getTag')->setName('tag');
+$app->group('/tag', function() {
+    $this->get('s', TagController::class.':getTags')->setName('tags');
+    $this->get('s.json', TagController::class.':getTagsJson')->setName('tags-json');
+    $this->get('/{tag}', TagController::class.':getTag')->setName('tag');
+});
 
-$app->post('/transaction[/{id}]', TransactionController::class.':postTransaction')->setName('transaction-post');
-$app->get('/transaction/new', TransactionController::class.':getTransactionNewForm')->setName('transaction-new');
-$app->get('/transaction/{id}/edit', TransactionController::class.':getTransactionEditForm')->setName('transaction-edit');
-$app->get('/transaction/{id}', TransactionController::class.':getTransaction')->setName('transaction');
-$app->get('/transactions[/{account}]', TransactionController::class.':getTransactionsForAccount')->setName('transactions');
-$app->get('/transactions/{year}/{month}', TransactionController::class.':getTransactionsForMonth')->setName('month');
+$app->group('/transaction', function() {
+    $this->post('[/{id}]', TransactionController::class.':postTransaction')->setName('transaction-post');
+    $this->get('/new', TransactionController::class.':getTransactionNewForm')->setName('transaction-new');
+    $this->get('/{id}/edit', TransactionController::class.':getTransactionEditForm')->setName('transaction-edit');
+    $this->get('/{id}', TransactionController::class.':getTransaction')->setName('transaction');
+    $this->get('s[/{account}]', TransactionController::class.':getTransactionsForAccount')->setName('transactions');
+    $this->get('s/{year}/{month}', TransactionController::class.':getTransactionsForMonth')->setName('month');
+});
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // AUTH
